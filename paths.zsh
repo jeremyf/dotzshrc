49f8cef2ab5ac7a58dd183c3716a7f5b1d05764c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-02-13 14:13:02 -0500

    Adding mysql bin to path

--- paths.zsh
+++ paths.zsh
@@ -1,3 +1,9 @@
+if [ -d /usr/local/opt/mysql@5.7/bin ]; then
+  echo "$PATH" | grep -q "/usr/local/opt/mysql@5.7/bin:" || export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
+  # Needed for building mysql gem
+  export LDFLAGS="-L/usr/local/opt/mysql@5.7/lib"
+  export CPPFLAGS="-I/usr/local/opt/mysql@5.7/include"
+fi
 if [ -d /usr/local/opt/openssl@1.1/bin ]; then
   echo "$PATH" | grep -q "/usr/local/opt/openssl@1.1/bin:" || export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
 fi
