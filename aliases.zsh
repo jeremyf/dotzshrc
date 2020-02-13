66173c5f7ab9c8a5be8a47cac9e540be30372b7d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-02-13 14:12:30 -0500

    Updating aliases to not use sudo

--- aliases.zsh
+++ aliases.zsh
@@ -16,7 +16,7 @@ alias psx="ps ax | ag $1"
 # Hyrax aliases
 alias hyrax-devup='fcrepo_wrapper & solr_wrapper & redis-server &'
 alias hyrax-testup='fcrepo_wrapper --config config/fcrepo_wrapper_test.yml & solr_wrapper --config config/solr_wrapper_test.yml & redis-server &'
-alias hyrax-devdown='sudo pkill -f solr_wrapper & sudo pkill -f fcrepo_wrapper & redis-cli shutdown'
+alias hyrax-devdown='pkill -f solr_wrapper & pkill -f fcrepo_wrapper & redis-cli shutdown'
 
 # SSH Tunnel:
 # ssh libvirt6.library.nd.edu -L 8080:localhost:8080
