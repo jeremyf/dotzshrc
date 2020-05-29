4a2171a0c7aa6b9c4831248122de557d7bab66bf
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-05-28 20:48:29 -0400

    Commenting out VS Code configuration
    I removed VS Code and don't wnat to have this polluting my install.

--- install.rb
+++ install.rb
@@ -25,17 +25,17 @@ Dir.glob(bin_sources).each do |source_filename|
 end
 $stdout.puts "Finished installing bin aliases…"
 
-$stdout.puts "Installing VS Code symlinks"
-vscode = File.expand_path("../vscode/*", __FILE__)
-target_directory = File.join(home_dirname, "Library/Application\ Support/Code/User")
-Dir.glob(vscode).each do |source_filename|
-  target_name = File.join(target_directory, File.basename(source_filename))
-  if File.directory?(source_filename)
-    # Without this line, the source directory will be linked as a sub-directory
-    # of the target. Which really isn't what we want.
-    FileUtils.rm_rf(target_name)
-  end
-  $stdout.puts "\t#{target_name} ->\n\t\t#{source_filename}"
-  FileUtils.ln_sf(source_filename, target_name)
-end
-$stdout.puts "Finished installing VS Code symlinks…"
+# $stdout.puts "Installing VS Code symlinks"
+# vscode = File.expand_path("../vscode/*", __FILE__)
+# target_directory = File.join(home_dirname, "Library/Application\ Support/Code/User")
+# Dir.glob(vscode).each do |source_filename|
+#   target_name = File.join(target_directory, File.basename(source_filename))
+#   if File.directory?(source_filename)
+#     # Without this line, the source directory will be linked as a sub-directory
+#     # of the target. Which really isn't what we want.
+#     FileUtils.rm_rf(target_name)
+#   end
+#   $stdout.puts "\t#{target_name} ->\n\t\t#{source_filename}"
+#   FileUtils.ln_sf(source_filename, target_name)
+# end
+# $stdout.puts "Finished installing VS Code symlinks…"
