7acc6cd365ba2075a25aff7963d0c58eb0766e7b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-06-19 10:44:18 -0400

    Adding handling of gnu-linux

--- configs/paths.zsh
+++ configs/paths.zsh
@@ -1,31 +1,38 @@
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Check if I have homebrew installed; If so, add this to the path
+    if [ -d "/home/linuxbrew/.linuxbrew/bin" ]; then
+        export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+    fi
+fi
+    
 if [ -d /usr/local/opt/mysql@5.7/bin ]; then
-  export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
-  # Needed for building mysql gem
-  # export LDFLAGS="-L/usr/local/opt/mysql@5.7/lib"
-  # export CPPFLAGS="-I/usr/local/opt/mysql@5.7/include"
+    export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
+    # Needed for building mysql gem
+    # export LDFLAGS="-L/usr/local/opt/mysql@5.7/lib"
+    # export CPPFLAGS="-I/usr/local/opt/mysql@5.7/include"
 fi
 if [ -d /usr/local/opt/libxml2/bin ]; then
-  export PATH="/usr/local/opt/libxml2/bin:$PATH"
-  # export LDFLAGS="-L/usr/local/opt/libxml2/lib"
-  # export CPPFLAGS="-I/usr/local/opt/libxml2/include"
+    export PATH="/usr/local/opt/libxml2/bin:$PATH"
+    # export LDFLAGS="-L/usr/local/opt/libxml2/lib"
+    # export CPPFLAGS="-I/usr/local/opt/libxml2/include"
 fi
 
 # Ensuring homebrew install of python is selected over OSX's
 # ancient python
 if [ -d "/usr/local/opt/python/libexec/bin" ]; then
-  export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+    export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 fi
 
 if [ -d "/usr/local/opt/icu4c/bin" ]; then
-  export PATH="/usr/local/opt/icu4c/bin:$PATH"
+    export PATH="/usr/local/opt/icu4c/bin:$PATH"
 fi
 
 if [ -d "/usr/local/opt/icu4c/sbin" ]; then
-  export PATH="/usr/local/opt/icu4c/sbin:$PATH"
+    export PATH="/usr/local/opt/icu4c/sbin:$PATH"
 fi
 
 if [ -d "/usr/local/Cellar/graphviz/2.44.0/bin" ]; then
-  export PATH="/usr/local/Cellar/graphviz/2.44.0/bin:$PATH"
+    export PATH="/usr/local/Cellar/graphviz/2.44.0/bin:$PATH"
 fi
 
 # This helps in building out Ruby using openssl
@@ -34,30 +41,30 @@ export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
 
 # I'm not certain if this is a good idea or not
 if [ -d /usr/local/opt/openssl@1.1/bin ]; then
-  export PATH="$(brew --prefix openssl@1.1)/bin:$PATH"
+    export PATH="$(brew --prefix openssl@1.1)/bin:$PATH"
 fi
 
 if [ -d $HOME/.emacs.d/bin ]; then
-  export PATH="$HOME/.emacs.d/bin:$PATH"
+    export PATH="$HOME/.emacs.d/bin:$PATH"
 fi
 
 if [ -d $HOME/bin ]; then
-  export PATH="$HOME/bin:$PATH"
+    export PATH="$HOME/bin:$PATH"
 fi
 
 if [ -d /usr/local/sbin ]; then
-  echo "$PATH" | grep -q "/usr/local/sbin:" || export PATH="/usr/local/sbin:$PATH"
+    echo "$PATH" | grep -q "/usr/local/sbin:" || export PATH="/usr/local/sbin:$PATH"
 fi
 
 if [ -d /usr/local/opt/coreutils/libexec/gnubin ]; then
-  export PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
-  export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:${MANPATH}"
+    export PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
+    export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:${MANPATH}"
 fi
 
 export GOROOT=/usr/local/opt/go/libexec/
 export GOPATH=$HOME/code_of_the_go
 if [ -d $GOPATH ]; then
-  echo "$PATH" | grep -q "$GOPATH" || export PATH="$PATH:$GOPATH/bin"
+    echo "$PATH" | grep -q "$GOPATH" || export PATH="$PATH:$GOPATH/bin"
 fi
 
 export LYNX_CFG="$HOME/.lynx.cfg"
