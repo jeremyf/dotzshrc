04f8a87ce4eb2a753471d48a0fc44a15d861c339
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-06-19 20:58:14 -0400

    Adding script to install git helpers by OS X
    In .gitconfig you can't have ENV variables.  However --system settings
    can be separate from the --global config (e.g. the ~/.gitconfig) file.

--- install.rb
+++ install.rb
@@ -28,3 +28,15 @@ Dir.glob(bin_sources).each do |source_filename|
   FileUtils.ln_sf(source_filename, target_name)
 end
 $stdout.puts 'Finished installing bin aliases…'
+
+if RUBY_PLATFORM.include?('darwin')
+  $stdout.puts "Installing global git config for darwin"
+  system("git config --system --add credential.helper osxkeychain")
+  system("git config --system --add interactive.diffFilter `brew --prefix git`/share/git-core/contrib/diff-highlight/diff-highlight")
+  system("git config --system --add core.pager `brew --prefix git`/share/git-core/contrib/diff-highlight/diff-highlight | less -F -X")
+elsif RUBY_PLATFORM.include?('linux')
+  $stdout.puts "Installing global git config for gnu-linux"
+  system("git config --system --add credential.helper /usr/lib/git-core/git-credential-libsecret")
+  system("git config --system --add interactive.diffFilter /usr/share/git/diff-highlight/diff-highlight")
+  system("git config --system --add core.pager /usr/share/git/diff-highlight/diff-highlight | less -F -X")
+end
