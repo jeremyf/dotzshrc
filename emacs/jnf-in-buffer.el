45682f4a41591983713e2d6f11f2b86659c56e33
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-10-16 22:42:31 -0400

    Adding hippie-expand

--- emacs/jnf-in-buffer.el
+++ emacs/jnf-in-buffer.el
@@ -32,6 +32,20 @@
   :straight t
   :hook (prog-mode . yafolding-mode))
 
+;; Using Hippie expand, I toggle through words already referenced.
+(setq hippie-expand-try-functions-list '(try-expand-dabbrev-visible
+                                         try-expand-dabbrev
+                                         try-expand-list
+                                         try-expand-all-abbrevs
+                                         try-expand-dabbrev-all-buffers
+                                         try-expand-dabbrev-from-kill
+                                         try-complete-file-name-partially
+                                         try-complete-file-name
+                                         try-complete-lisp-symbol-partially
+                                         try-complete-lisp-symbol
+                                         ))
+(global-set-key (kbd "M-SPC") 'hippie-expand)
+
 ;; Expand or contract point/region to next logical element.
 (use-package expand-region
   :straight t
