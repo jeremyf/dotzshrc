3732119ca2d4d6e0a02210ff9f2b93767d41cf65
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-10-21 17:22:34 -0400

    Warning at git-commit-summary-max-length 50 chars
    Prior to this commit, it was using the default 68

--- emacs/jnf-git.el
+++ emacs/jnf-git.el
@@ -11,9 +11,8 @@
 ;; The OMG awesome git client for emacs.
 (use-package magit
   :straight t
-  :init (setq git-commit-fill-column 72) ;; Adding format to
-                                         ;; git-commit-fill-column of
-                                         ;; 72 as best practice.
+  :init (setq git-commit-fill-column 72) ;; Adding format to git-commit-fill-column of 72 as best practice.
+  (setq git-commit-summary-max-length 50)
   :bind (("H-g" . magit-status)
          ("C-M-g" . magit-status)))
 
@@ -49,16 +48,6 @@
 (use-package git-link
 	     :config (setq git-link-use-commit t) ;; URL will be SHA instead of branch
 	     :straight t)
-
-;; Copied from
-;; https://github.com/magit/magit/blob/9423edc0b311117ab5fe87457c3e01c7db22a3c7/lisp/git-commit.el
-;; And set to 50 instead of 68, because good form is 50 characters.
-(defcustom git-commit-summary-max-length 50
-  "A reminder of an aspirational goal of terse git commit summary
-lines."
-  :group 'git-commit
-  :safe 'numberp
-  :type 'number)
 
 (provide 'jnf-git.el)
 ;;; jnf-git.el ends here
