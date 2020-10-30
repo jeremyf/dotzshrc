b943c2329a51d9757a51f9abab3ea08aa088b66a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-10-30 17:30:31 -0400

    Synchronizing modus-themes

--- emacs/jnf-display.el
+++ emacs/jnf-display.el
@@ -19,76 +19,60 @@
 
 (use-package modus-operandi-theme
   :straight t
-  :init
-  (setq modus-operandi-theme-slanted-constructs t
-        modus-operandi-theme-bold-constructs t
-        modus-operandi-theme-fringes 'subtle
-        modus-operandi-theme-mode-line '3d
-        modus-operandi-theme-syntax 'yellow-comments-green-strings
-        modus-operandi-theme-intense-paren-match t
-        modus-operandi-theme-links 'faint
-        modus-operandi-theme-prompts 'intense
-        modus-operandi-theme-completions 'opinionated
-        modus-operandi-theme-diffs 'desaturated
-        modus-operandi-theme-org-blocks 'rainbow
-        modus-operandi-theme-scale-headings t)
   :config
   (defadvice load-theme (before theme-dont-propagate activate)
     "Disable theme before loading new one."
-    (mapc #'disable-theme custom-enabled-themes))
-
-  (load-theme 'modus-operandi t))
+    (mapc #'disable-theme custom-enabled-themes)))
 
 (use-package modus-vivendi-theme
-  :init
-  (setq modus-vivendi-theme-slanted-constructs t
-        modus-vivendi-theme-bold-constructs t
-        modus-vivendi-theme-fringes 'subtle
-        modus-vivendi-theme-mode-line '3d
-        modus-vivendi-theme-syntax 'yellow-comments-green-strings
-        modus-vivendi-theme-intense-paren-match t
-        modus-vivendi-theme-links 'faint
-        modus-vivendi-theme-prompts 'intense
-        modus-vivendi-theme-completions 'opinionated
-        modus-vivendi-theme-diffs 'desaturated
-        modus-vivendi-theme-org-blocks 'rainbow
-        modus-vivendi-theme-scale-headings t)
   :straight t)
 
-;; At present, I have not figured out enough about Emacs to remove the
-;; duplication present in the use-package modus-operandi-them and the
-;; below font-lock configuration.
-;;
-;; Note, I have not added font lock declarations to my
-;; modus-vivendi-theme, as I default to modus-operandi and use the
-;; toggle below to switch to vivendi.  Were I to default to vivendi,
-;; I'd need to make some updates to the config.
+(defmacro modus-themes-format-sexp (sexp &rest objects)
+  "A macro to evaluate the modus themes configuration."
+  `(eval (read (format ,(format "%S" sexp) ,@objects))))
+
+;; This `dolist' keeps synchronized the `modus-operandi' and
+;; `modus-vivendi'.
+(dolist (theme '("operandi" "vivendi"))
+  (modus-themes-format-sexp
+   (defun modus-%1$s-theme-load ()
+     (setq modus-%1$s-theme-slanted-constructs t
+           modus-%1$s-theme-bold-constructs t
+           modus-%1$s-theme-fringes 'subtle
+           modus-%1$s-theme-mode-line '3d
+           modus-%1$s-theme-syntax 'yellow-comments-green-strings
+           modus-%1$s-theme-intense-hl-line nil
+           modus-%1$s-theme-intense-paren-match t
+           modus-%1$s-theme-links 'faint
+           modus-%1$s-theme-no-mixed-fonts nil
+           modus-%1$s-theme-prompts 'intense
+           modus-%1$s-theme-completions 'opinionated
+           modus-%1$s-theme-diffs 'desaturated
+           modus-%1$s-theme-org-blocks 'rainbow
+           modus-%1$s-theme-variable-pitch-headings nil
+           modus-%1$s-theme-scale-headings t
+           modus-%1$s-theme-scale-1 1.1
+           modus-%1$s-theme-scale-2 1.15
+           modus-%1$s-theme-scale-3 1.21
+           modus-%1$s-theme-scale-4 1.27
+           modus-%1$s-theme-scale-5 1.33)
+     (load-theme 'modus-%1$s t))
+   theme))
+
 (defun modus-themes-toggle ()
   "Toggle between `modus-operandi' and `modus-vivendi' themes."
   (interactive)
   (if (eq (car custom-enabled-themes) 'modus-operandi)
       (progn
         (disable-theme 'modus-operandi)
-        (load-theme 'modus-vivendi t)
-        ;; (set-background-color "#172637") ;; Background color for dark theme TakeOnRules.com
-        )
+        (modus-vivendi-theme-load))
     (disable-theme 'modus-vivendi)
-    (load-theme 'modus-operandi t)
-    ;; (set-background-color "#fffff8") ;; Background color for light theme TakeOnRules.com
-    ))
+    (modus-operandi-theme-load)))
+
+(modus-operandi-theme-load)
 
 (global-set-key (kbd "<s-clear>") 'modus-themes-toggle)
 (global-set-key (kbd "s-9") 'modus-themes-toggle)
-
-;; I really like the background colors of my blog (light and dark
-;; mode).  I'm using this to over-write the background colors.  I
-;; don't not believe this to be the recommended way for Modus themes,
-;; but it's what I've found to work.
-;;
-;; As the theme continues to move toward v1.0, I'd imagine more clear
-;; instructions will emerge.  It turns out, I need more Elisp practice.
-;; (add-to-list 'default-frame-alist '(background-color . "#fffff8"))
-;; (set-background-color "#fffff8")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; BEGIN BLOCK
