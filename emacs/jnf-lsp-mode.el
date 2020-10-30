f87aacd9bf23bcbfd6b79fdc690a91c4dc90e2ed
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-10-30 15:26:53 -0400

    Adding company lsp support
    Let's see how this works

--- emacs/jnf-lsp-mode.el
+++ emacs/jnf-lsp-mode.el
@@ -12,19 +12,27 @@
   :hook ((ruby-mode . lsp))
   :commands (lsp))
 
-(use-package lsp-ivy
-  :straight t
-  :commands lsp-ivy-workspace-symbol)
 
 ;; This package provides some nice UI behavior for documentation and linting
 ;;
 ;; In particular, I like 'lsp-ui-peek-find-reference
 (use-package lsp-ui
+  :after lsp-mode
   :commands lsp-ui-mode
   :hook ((ruby-mode . lsp-ui-mode)
          (ruby-mode . lsp-ui-peek-mode)
          (ruby-mode . lsp-ui-sideline-mode))
   :straight t)
+
+(use-package lsp-ivy
+  :after ivy
+  :straight t
+  :commands lsp-ivy-workspace-symbol)
+
+(use-package company-lsp
+  :straight t
+  :custom (company-lsp-enable-snippet t)
+  :after (company lsp-mode))
 
 ;; Solargraph is the language tool for lsp-mode and Ruby
 (use-package solargraph
