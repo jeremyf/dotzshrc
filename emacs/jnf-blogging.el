dba6a8bf1fd9c5abd832fe9c35bb62129a61f73b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-11-01 16:04:30 -0500

    Adding `tor-post-new` function
    Note, this is naive in its YAML creation.  It turns out there aren't a
    lot of good options for Emacs Lisp and YAML.  But this should do

--- emacs/jnf-blogging.el
+++ emacs/jnf-blogging.el
@@ -6,7 +6,7 @@
 ;;
 ;;  Todo:
 ;;
-;;  - [ ] tor-post-new :: a function for creating a new post
+;;  - [X] tor-post-new :: a function for creating a new post
 ;;  - [ ] tor-tags :: a function for prompting for existing tags
 ;;
 ;;; Code:
@@ -14,19 +14,31 @@
 
 ;; TODO - Complete this function.
 ;; - [X] Prompt for and build proper filename
-;; - [ ] Create named file
-;; - [ ] Populate named file with pre-amble
+;; - [X] Create named file
+;; - [X] Populate named file with pre-amble
 (defun tor-post-new (title &optional)
-  "Create a new draft blog post for the prompted TITLE."
+  "Create and visit a new draft blog post for the prompted TITLE.
+
+The file for the blog post conforms to the path schema of posts for TakeOnRules.com."
   (interactive (list (read-from-minibuffer
 		      "Title: "
 		      nil nil nil nil)))
 
   (let ((default-directory (concat "~/git/takeonrules.github.io/content/posts/"
                                    (format-time-string "%Y/"))))
-    (let ((filename (concat default-directory (s-dashed-words title) ".md")))
-      (message filename)
-    )))
-
+    (let ((fpath (concat default-directory (s-dashed-words title) ".md"))
+          (slug (s-dashed-words title)))
+      (write-region (concat
+                     "---"
+                     "\ndate: " (format-time-string "%Y-%m-%d %H:%M:%S %Z")
+                     "\ndraft: true"
+                     "\nlicenses:\n- by-nc-nd-4_0"
+                     "\nslug: " slug
+                     "\ntitle: " title
+                     "\ntype: post"
+                     "\n---\n")
+                    nil (expand-file-name fpath) nil nil nil t)
+      (find-file (expand-file-name fpath))
+      )))
 (provide 'jnf-blogging.el)
 ;;; jnf--blogging.el ends here
