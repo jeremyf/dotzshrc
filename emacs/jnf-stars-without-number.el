6b3c9716aa69a12985e97ad99203a8975114c7c4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-11-08 19:34:07 -0500

    Extracting the logic for thel-sector-filename

--- emacs/jnf-stars-without-number.el
+++ emacs/jnf-stars-without-number.el
@@ -1,5 +1,5 @@
-;;; -*- lexical-binding: t; -*-
 ;;; jnf-stars-without-number.el --- Summary
+;;; -*- lexical-binding: t; -*-
 ;;
 ;;; Commentary:
 ;;
@@ -7,8 +7,19 @@
 ;;
 ;;; Code:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun thel-sector-filename (title)
+  "Convert the given TITLE to a filename."
+  (f-join org-directory
+          "projects"
+          "thel-sector"
+          (concat (format-time-string "%Y%m%d---") (s-snake-case title) ".org")))
+
 (defun swn-npc (culture &optional)
-  "Capture a random `swnt' NPC with the given CULTURE in the Thel Sector org project."
+  "Capture a random `swnt' npc of the prompted CULTURE.
+
+This function writes to the as-of-now hard-coded Thel Sector
+project in my org-directory."
   (interactive (list (completing-read "Culture: " '(
                                                     ("Arabic" 1)
                                                     ("Chinese" 2)
@@ -24,7 +35,7 @@
          (npc-name (string-trim (replace-regexp-in-string ":" "" (car npc-string-list))))
          (body (concat "#+title: " npc-name "\n\n* " npc-name "\n\n" (string-join npc-string-list "\n")))
          ;; Conforms to the capture template
-         (fpath (f-join org-directory "projects" "thel-sector" (concat (format-time-string "%Y%m%d---") (s-snake-case npc-name) ".org"))))
+         (fpath (thel-sector-filename npc-name)))
       (write-region body nil fpath nil nil nil t)
       (insert (concat "[[file:" fpath "][" npc-name "]]"))
       (org-roam-db-build-cache)))
