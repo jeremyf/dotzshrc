b0c84258c2012827f2895a745dbc04761ce22442
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-11-09 09:45:42 -0500

    Refinement of Centaur Tabs behavior
    I'm okay with different projects bleeding into the same Tab Group.  This
    change sets up 4 tab groups:
    
    - RSS :: My RSS feed
    - System :: The *message* and *scratch* type buffers
    - Help :: When I request help
    - Editor :: Everything else"

--- emacs/jnf-tabs.el
+++ emacs/jnf-tabs.el
@@ -27,61 +27,35 @@
         centaur-tabs-cycle-scope 'tabs
         centaur-tabs-set-bar 'under)
   (centaur-tabs-headline-match)
-  (defun centaur-tabs-buffer-groups ()
-    "`centaur-tabs-buffer-groups' control buffers' group rules.
+  (defun jnf-centaur-tabs-buffer-groups ()
+    "A function to define the centaur tabs's buffer group rules.
 
- Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
- All buffer name start with * will group to \"Emacs\".
- Other buffer group by `centaur-tabs-get-group-name' with project name."
+There are four groups:
+
+- RSS :: My RSS feed
+- System :: The *message* and *scratch* type buffers
+- Help :: When I request help
+- Editor :: Everything else"
     (list
      (cond
       ((string-equal "*el" (substring (buffer-name) 0 3)) "RSS")
-      ((or (string-equal "*" (substring (buffer-name) 0 1))
-	   (memq major-mode '(magit-process-mode
-			      magit-status-mode
-			      magit-diff-mode
-			      magit-log-mode
-			      magit-file-mode
-			      magit-blob-mode
-			      magit-blame-mode
-			      )))
-       "Emacs")
-      ((derived-mode-p 'prog-mode)
-       "Editing")
-      ((derived-mode-p 'dired-mode)
-       "Dired")
-      ((memq major-mode '(helpful-mode
-			  help-mode))
-       "Help")
-      ((memq major-mode '(org-mode
-			  org-agenda-clockreport-mode
-			  org-src-mode
-			  org-agenda-mode
-			  org-beamer-mode
-			  org-indent-mode
-			  org-bullets-mode
-			  org-cdlatex-mode
-			  org-agenda-log-mode
-			  diary-mode))
-       "Editing")
-      (t
-       (centaur-tabs-get-group-name (current-buffer))))))
+      ((string-equal "*" (substring (buffer-name) 0 1)) "System")
+      ((memq major-mode '(helpful-mode help-mode)) "Help")
+      (t "Editor"))))
   :custom
-  (centaur-tabs-gray-out-icons 'buffer)
   (centaur-tabs-change-fonts "JetBrains Mono" 90)
+  (centaur-tabs-height 30)
+  (centaur-tabs-gray-out-icons 'buffer)
   (centaur-tabs-style "rounded")
-  (centaur-tabs-height 36)
-  (centaur-tabs-set-icons t)
   (centaur-tabs-set-modified-marker t)
   (centaur-tabs-modified-marker "●")
-  ;; (centaur-tabs-buffer-groups-function #'centaur-tabs-projectile-buffer-groups)
+  (centaur-tabs-buffer-groups-function #'jnf-centaur-tabs-buffer-groups)
   :bind (("s-{" . #'centaur-tabs-backward)
          ("s-}" . #'centaur-tabs-forward)
          ("C-c C-5". #'centaur-tabs-extract-window-to-new-frame)
          ([s-up] . #'centaur-tabs-backward-group)
          ([s-down] . #'centaur-tabs-forward-group)
-         ("C-s-t" . #'centaur-tabs-counsel-switch-group)
-         ("C-c C-o" . #'centaur-tabs-open-directory-in-external-application)))
+         ("C-s-t" . #'centaur-tabs-counsel-switch-group)))
 
 
 (provide 'jnf-tabs.el)
