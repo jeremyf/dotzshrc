57354c6831a7e408c96642342e27af70b2033709
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-11-11 16:06:21 -0500

    Removing tide (instead favor lsp-mode)

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -307,31 +307,6 @@
 (use-package typescript-mode
   :straight t
   :mode (("\\.ts\\'" . typescript-mode)))
-
-;; Command `M-.` to jump to definition
-;; Command `M-,` to jump back from the definition
-(use-package tide
-  :straight t)
-
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  ;; company is an optional dependency. You have to
-  ;; install it separately via package-install
-  ;; `M-x package-install [ret] company`
-  (company-mode +1))
-
-;; aligns annotation to the right hand side
-(setq company-tooltip-align-annotations t)
-
-;; formats the buffer before saving
-(add-hook 'before-save-hook 'tide-format-before-save)
-
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
 ;; END Typescript
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
