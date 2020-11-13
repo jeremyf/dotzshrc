ebec45a8de4f1af21857cbb166d434b7882ee5f8
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-11-12 22:42:12 -0500

    Adding company-emojify

--- emacs/jnf-in-buffer.el
+++ emacs/jnf-in-buffer.el
@@ -170,6 +170,25 @@
   :bind (:map company-active-map ("C-c h" . 'company-quickhelp-manual-begin))
   :config (company-quickhelp-mode))
 
+(use-package company-emoji
+  :straight (company-emoji :type git :host github :repo "dunn/company-emoji" :branch "trunk")
+  :config (add-to-list 'company-backends 'company-emoji))
+
+(defun --set-emoji-font (frame)
+  "Adjust the font settings of FRAME so Emacs can display emoji properly."
+  (if (eq system-type 'darwin)
+      ;; For NS/Cocoa
+      (set-fontset-font t 'symbol (font-spec :family "Apple Color Emoji") frame 'prepend)
+    ;; For Linux
+    (set-fontset-font t 'symbol (font-spec :family "Symbola") frame 'prepend)))
+
+;; For when Emacs is started in GUI mode:
+(--set-emoji-font nil)
+;; Hook for when a frame is created with emacsclient
+;; see https://www.gnu.org/software/emacs/manual/html_node/elisp/Creating-Frames.html
+(add-hook 'after-make-frame-functions '--set-emoji-font)
+
+
 (defun jnf/kill-region-or-backward-word ()
   (interactive)
   (if (region-active-p)
