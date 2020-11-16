746ac4c7f8bbf28c87e16904bc844c53ac0d6ec8
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-11-16 08:05:44 -0500

    Removing some of magit's chatter
    From https://jakemccrary.com/blog/2020/11/14/speeding-up-magit/

--- emacs/jnf-git.el
+++ emacs/jnf-git.el
@@ -14,7 +14,24 @@
   :straight t
   :init (setq git-commit-fill-column 72) ;; Adding format to git-commit-fill-column of 72 as best practice.
   (setq git-commit-summary-max-length 50)
-  :bind (("C-c m" . magit-status)))
+  :bind ("C-c m" . magit-status)
+  :init
+  ;; Have magit-status go full screen and quit to previous
+  ;; configuration.  Taken from
+  ;; http://whattheemacsd.com/setup-magit.el-01.html#comment-748135498
+  ;; and http://irreal.org/blog/?p=2253
+  (defadvice magit-status (around magit-fullscreen activate)
+    (window-configuration-to-register :magit-fullscreen)
+    ad-do-it
+    (delete-other-windows))
+  (defadvice magit-quit-window (after magit-restore-screen activate)
+    (jump-to-register :magit-fullscreen))
+  :config
+  (remove-hook 'magit-status-sections-hook 'magit-insert-tags-header)
+  (remove-hook 'magit-status-sections-hook 'magit-insert-status-headers)
+  (remove-hook 'magit-status-sections-hook 'magit-insert-unpulled-from-pushremote)
+  (remove-hook 'magit-status-sections-hook 'magit-insert-unpulled-from-upstream)
+  (remove-hook 'magit-status-sections-hook 'magit-insert-unpushed-to-upstream-or-recent))
 
 (use-package forge
   :straight t)
