e4e68178bbf715f71c9b731f10a8fbade70a8bd3
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-04-25 20:53:24 -0400

    Removing pdf-tools as I don't need it

--- emacs/darwin/emacs-config.el
+++ emacs/darwin/emacs-config.el
@@ -39,22 +39,6 @@
          (set-fontset-font
           t 'symbol (font-spec :family "Apple Color Emoji") nil 'prepend))
 
-;; Emacs comes with DocView built in.  pdf-tools is a replacement for
-;; DocView.  I've found the rendered images a bit more crisp and the
-;; interactions a bit more responsive.  However, I have not been able
-;; to get `org-noter' working with `pdf-tools'.  `org-noter' provides
-;; annotation services for PDFs.
-(use-package pdf-tools
-  :pin manual ;; manually update
-  :straight t
-  :defer t
-  :ensure t
-  :config (pdf-tools-install) ;; initialise
-  (setq-default pdf-view-display-size 'fit-page) ;; open pdfs scaled to fit page
-  (setq pdf-annot-activate-created-annotations t) ;; automatically annotate highlights
-  (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward);; use normal isearch
-  )
-
 ;; Keep this after `counsel' so that the key binding is
 ;;  overridden only on OSX."
 (use-package counsel-osx-app
