a9a34ca4d16c0228cf64a4c0a145b3ed11011553
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-04-29 17:32:54 -0400

    Adding elisp-experiments
    This is included so I have history of things I've done (e.g. lessons
    I've practiced).

---
+++ emacs/elisp-experiments.el
@@ -0,0 +1,42 @@
+;;; elisp-experiments.el --- Summary -*- lexical-binding: t; -*-
+;;
+;;; Commentary:
+;;
+;;  This "package" contains experiments of practice.
+;;
+;;; Code:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Call the open-pdf-to-page with a known PDF.  The goal is to ensure
+;; that the shell script works reasonably well.
+(shell-command "open-pdf-to-page ~/Documents/RPGs/Warhammer-Fantasy-RPG-WHFRPG/CB72400-Warhammer-Fantasy-Roleplay-4E-WHFRPG.pdf")
+
+;; In this function, I want to find an org file that has a matching
+;; substring; which is how org-noter builds it's annotations.
+;;
+;; Find file with NOTER_DOCUMENT, this aligns with the org-noter
+;; behavior.  Note, ripgrep exits with a non-zero if it fails to find
+;; the file.
+(shell-command "rg \":NOTER_DOCUMENT: WFRP-Enemy-in-the-Shadows-v-1.02.pdf\" ~/Documents/RPGs/Warhammer-Fantasy-RPG-WHFRPG/The-Enemy-within-Campaign/ --files-with-matches --glob \"*.org\"")
+
+;; Write an org file to annotate the given filename
+(let* ((filename "~/Documents/RPGs/Warhammer-Fantasy-RPG-WHFRPG/The-Enemy-within-Campaign/WFRP-Enemy-in-the-Shadows-v-1.02.pdf")
+       (title (file-name-base filename))
+       (annotation_file_path (concat "~/git/org/annotations/" (format-time-string "%Y%m%d---") (s-dashed-words title) ".org")))
+  (write-region (concat
+                 "#+title: " title
+                 "\n#+roam_tags:"
+                 "\n* " title
+                 "\n  :PROPERTIES:"
+                 "\n  :NOTER_DOCUMENT: " annotation_file_path
+                 "\n  :END:"
+                 "\n\n** Begin Topic"
+                 "\n   :PROPERTIES:"
+                 "\n   :NOTER_PAGE:"
+                 "\n   :END:"
+                 )
+                nil (expand-file-name annotation_file_path) nil nil nil t)
+  (find-file (expand-file-name annotation_file_path)))
+
+(provide 'elisp-experiments.el)
+;;; elisp-experiments.el ends here
