72d08e6f876658554ff020ff0b3b7e3ac6842c8d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-05-11 14:22:25 -0400

    Extracting a modes file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -61,8 +61,7 @@
   )
 
 
-;; (require 'jnf-ivy.el)
-(require 'jnf-selectrum.el)  ;;; Still working on this swap out
+(require 'jnf-selectrum.el)
 
 ;; https://www.emacswiki.org/emacs/RecentFiles#h5o-1
 ;; Save recentf list every five minutes
@@ -136,62 +135,11 @@
       )
 
 (require 'jnf-epub.el)
-
-(use-package emmet-mode
-  :straight t
-  :bind (("C-c C-e" . emmet-expand-yas ))
-  :hook ((sgml-mode . emmet-mode)
-         (html-mode . emmet-mode)
-         (css-mode . emmet-mode)))
-
-;; https://github.com/jrblevin/markdown-mode/
-(use-package markdown-mode
-  :straight t
-  ;; :hook ((markdown-mode . turn-on-visual-line-mode))
-  :mode (("README\\.md\\'" . gfm-mode)
-         ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "/usr/local/bin/pandoc"))
-
-;; https://github.com/AdamNiederer/vue-mode
-(use-package vue-mode
-  :straight t
-  :mode (("\\.vue\\'" . vue-mode)))
-
-(use-package web-mode
-  :straight t
-  :config (setq web-mode-markup-indent-offset 2
-                web-mode-css-indent-offset 2
-                web-mode-code-indent-offset 2))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-
-
-
-(use-package yaml-mode
-  :straight t)
-
-(use-package plantuml-mode
-  :config (setq plantuml-executable-path (concat (getenv "HB_PATH") "/bin/plantuml")
-                plantuml-default-exec-mode 'executable
-                org-plantuml-executable-path (concat (getenv "HB_PATH") "/bin/plantuml")
-                org-plantuml-exec-mode 'executable)
-  :mode (("\\.plantuml\\'" . plantuml-mode))
-  :straight t)
+(require 'jnf-modes.el)
 
 ;; That letter is the beginning of a word. Narrow results from there.
 (use-package avy
   :straight t)
-
-(use-package json-mode
-  :straight t)
-
-;; Compressed JSON sure is ugly and illegible; This solves that
-;; problem.
-(use-package json-reformat
-  :straight t
-  :after json-mode
-  :init (setq json-reformat:indent-width 2))
 
 ;; Add a gopher and gemini client
 (use-package elpher
