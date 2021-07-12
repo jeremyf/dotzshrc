48f229be434788dbc8c90b4a42aad8f12d652ae2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-07-12 12:42:10 -0400

    Fixing broken method

--- emacs/jnf-modes.el
+++ emacs/jnf-modes.el
@@ -20,16 +20,15 @@
 
 (use-package web-mode
   :straight t
-  :bind (:map html-mode-map ("C-c t" . tor-cite-active-region-dwim))
+  :bind (:map html-mode-map ("C-c t" . jnf/tor-wrap-cite-active-region-dwim))
   :config (setq web-mode-markup-indent-offset 2
                 web-mode-css-indent-offset 2
                 web-mode-code-indent-offset 2))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 
-
-
 (use-package yaml-mode
+  :bind ("C-c t" . jnf/tor-glossary-entry)
   :straight t)
 
 (use-package plantuml-mode
