4a100d84aec21b59d6519f32d6aa37009a49404d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-07-18 15:58:28 -0400

    Working on org-roam-v2 capture

--- emacs/jnf-org-roam-v2.el
+++ emacs/jnf-org-roam-v2.el
@@ -6,6 +6,30 @@
 ;;
 ;;; Code:
 
+(defun jnf/org-roam-capture-templates ()
+  "Assign the capture templates.
+
+TODO: I would like to use a directory's files to assign these
+values.  However, my elisp skills are not yet up for that.  So I'm
+going get this working and seek help online."
+  (setq jnf/org-roam-capture-templates-list '(
+                    ("p" "Public" plain "%?"
+                                 :if-new (file+head "public/%<%Y%m%d>-${slug}.org"
+                                                    "#+title: ${title}\n#+FILETAGS: :public: %^G\n\n")
+                                 :unnarrowed t)
+                    ))
+  (add-to-list 'jnf/org-roam-capture-templates-list
+    '("n" "Notre Dame" plain "%?"
+     :if-new (file+head "notre-dame/%<%Y%m%d>-${slug}.org"
+                        "#+title: ${title}\n#+FILETAGS: :notre-dame: %^G\n\n")
+     :unnarrowed t))
+  jnf/org-roam-capture-templates-list)
+
+;; (defun jnf/capture-template-for-directory (dir)
+;;   (let* ((subdir (format "%s" (car (last (f-split dir)))))
+;;          (prefix (substring subdir 0 1)))
+;;     (list prefix (s-titleize subdir) plain)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; BEGIN ORG ROAM  and concerns
 ;;
@@ -18,17 +42,7 @@
   ;; Set more spaces for tags; As much as I prefer the old format,
   ;; this is the new path forward.
   (org-roam-node-display-template "${title:*} ${tags:40}")
-  (org-roam-capture-templates
-   '(
-     ("n" "Notre Dame" plain "%?"
-      :if-new (file+head "notre-dame/%<%Y%m%d>-${slug}.org"
-                         "#+title: ${title}\n#+FILETAGS: notre-dame\n\n")
-      :unnarrowed t)
-     ("p" "Public" plain "%?"
-      :if-new (file+head "public/%<%Y%m%d>-${slug}.org"
-                         "#+title: ${title}\n#+FILETAGS: public")
-      :unnarrowed t)
-     ))
+  (org-roam-capture-templates (jnf/org-roam-capture-templates))
   :bind (("C-c r l" . org-roam-buffer-toggle)
          ("C-c r f" . org-roam-node-find)
          ("C-c r g" . org-roam-graph)
