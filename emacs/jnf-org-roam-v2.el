24d7babba5dd1c44a7b4900eb6b6be2e7ba56c63
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-07-24 11:17:00 -0400

    Switching over to org-roam v2
    There's still more work to do, but I need to reflect on what that will
    be.

--- emacs/jnf-org-roam-v2.el
+++ emacs/jnf-org-roam-v2.el
@@ -36,24 +36,22 @@ going get this working and seek help online."
 ;; With the latest update of org-roam, things again behavior
 ;; correctly.  Now I can just load org-roam as part of my day to day
 (use-package org-roam
-  :straight (org-roam :type git :host github :repo "org-roam/org-roam" branch: "v2")
+  ;; :straight (org-roam :type git :host github :repo "org-roam/org-roam" branch: "v2")
+  :straight t
   :custom
-  (org-roam-directory (file-truename "~/git/org-roam"))
+  (org-roam-directory (file-truename "~/git/org"))
   ;; Set more spaces for tags; As much as I prefer the old format,
   ;; this is the new path forward.
   (org-roam-node-display-template "${title:*} ${tags:40}")
-  (org-roam-capture-templates (jnf/org-roam-capture-templates))
+  ;; (org-roam-capture-templates (jnf/org-roam-capture-templates))
   :bind (("C-c r l" . org-roam-buffer-toggle)
          ("C-c r f" . org-roam-node-find)
          ("C-c r g" . org-roam-graph)
          ("C-c r i" . org-roam-node-insert)
          ("C-c r c" . org-roam-capture)
          ;; Dailies
-         ("C-c r j" . org-roam-dailies-capture-today))
-  :config
-  (org-roam-setup)
-  (require 'org-roam-protocol))
+         ("C-c r j" . org-roam-dailies-capture-today)))
 
-
+(org-roam-setup)
 (provide 'jnf-org-roam-v2.el)
 ;;; jnf-org-roam-v2.el ends here
