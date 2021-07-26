24ea85be36fe60ba2c73b6d07e7814ca8c453d06
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-07-26 13:29:20 -0400

    Adding functions for thel-sector
    Next stop is to write some macros

--- emacs/jnf-org-roam-v2.el
+++ emacs/jnf-org-roam-v2.el
@@ -23,11 +23,21 @@
 		 :unnarrowed t)
        ))
 
+(defun jnf/org-roam-templates-for (&rest symbols)
+  "Return a list of `org-roam' templates for the given SYMBOLS."
+  (-map (lambda (symbol) (plist-get jnf/org-roam-capture-templates-plist symbol))
+        symbols))
 
 ;; A menu of common tasks for `org-roam'.
 (defvar jnf/org-subject-menu--title (with-faicon "book" "Org Subject Menu" 1 -0.05))
 (pretty-hydra-define jnf/org-subject-menu (:foreign-keys warn :title jnf/org-subject-menu--title :quit-key "q" :exit t)
   (
+   "Projects"
+   (
+    ("t +" jnf/org-roam--thel-sector--capture     "Thel Sector: Capture Node…")
+    ("t !" jnf/org-roam--thel-sector--node-insert "Thel Sector: Insert Node…")
+    ("t ?" jnf/org-roam--thel-sector--node-find   " └─ Find Node…")
+    )
    "Org Mode"
    (
     ("+" org-roam-capture               "Capture Node…")
@@ -38,26 +48,117 @@
     )
    ))
 
-(defun jnf/org-roam-templates-for (&rest symbols)
-  "Return a list of `org-roam' templates for the given SYMBOLS."
-  (-map (lambda (symbol) (plist-get jnf/org-roam-capture-templates-plist symbol))
-        symbols))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; BEGIN org-roam overrides that were very much a copy, paste, and then replace.
+(cl-defun jnf/org-roam-capture (&optional goto keys &key templates filter-fn)
+  "Launches an `org-capture' process for a new or existing note.
 
-(defun jnf/org-roam-capture--thel-sector (&optional goto)
-  "Capture a Thel Sector entry.
+This uses the templates defined at `org-roam-capture-templates'.
+Arguments GOTO and KEYS see `org-capture'.
+Argument TEMPLATES see `org-roam-capture-'.
+Argument FILTER-FN see `org-roam-node-read'.
 
-When GOTO is non-nil, go to the note without creating an entry."
+NOTE: This is a copy of the code from the original `org-roam-capture'."
+  (interactive "P")
+  (let ((node (org-roam-node-read nil filter-fn)))
+
+                       :keys keys
+                       :node node
+                       :templates templates
+                       :props '(:immediate-finish nil))))
+(advice-add 'org-roam-capture :override #'jnf/org-roam-capture)
+
+(cl-defun jnf/org-roam-node-insert (&optional filter-fn &key templates)
+  "Find an Org-roam node and insert (where the point is) an \"id:\" link to it.
+FILTER-FN is a function to filter out nodes: it takes an `org-roam-node',
+and when nil is returned the node will be filtered out.
+
+Argument TEMPLATES see `org-roam-capture-'.
+
+NOTE: This is a copy of the code from the original `org-roam-node-insert'."
   (interactive)
-  (org-roam-capture- :goto (when goto '(4))
-                     :node (org-roam-node-read
-                            ;; Initial text.  Use region if one is selected, otherwise the given word.
-                            (if (use-region-p)
-                                (buffer-substring-no-properties (region-beginning) (region-end))
-                              (thing-at-point 'word))
-                            ;; Filter function
-                            (lambda (node) (-contains-p (org-roam-node-tags node) "thel-sector")))
-                     :props '(:immediate-finish nil)
-                     :templates (jnf/org-roam-templates-for :thel-sector)))
+  (unwind-protect
+      ;; Group functions together to avoid inconsistent state on quit
+      (atomic-change-group
+        (let* (region-text
+               beg end
+               (_ (when (region-active-p)
+                    (setq beg (set-marker (make-marker) (region-beginning)))
+                    (setq end (set-marker (make-marker) (region-end)))
+                    (setq region-text (org-link-display-format (buffer-substring-no-properties beg end)))))
+               (node (org-roam-node-read region-text filter-fn))
+               (description (or region-text
+                                (org-roam-node-title node))))
+          (if (org-roam-node-id node)
+              (progn
+                (when region-text
+                  (delete-region beg end)
+                  (set-marker beg nil)
+                  (set-marker end nil))
+                (insert (org-link-make-string
+                         (concat "id:" (org-roam-node-id node))
+                         description)))
+            (org-roam-capture-
+             :node node
+             :templates templates
+             :props (append
+                     (when (and beg end)
+                       (list :region (cons beg end)))
+                     (list :insert-at (point-marker)
+                           :link-description description
+                           :finalize 'insert-link))))))
+    (deactivate-mark)))
+(advice-add 'org-roam-node-insert :override #'jnf/org-roam-node-insert)
+
+(cl-defun jnf/org-roam-node-find (&optional other-window initial-input filter-fn &key templates)
+  "Find and open an Org-roam node by its title or alias.
+
+INITIAL-INPUT is the initial input for the prompt.
+FILTER-FN is a function to filter out nodes: it takes an `org-roam-node',
+and when nil is returned the node will be filtered out.
+If OTHER-WINDOW, visit the NODE in another window.
+Argument TEMPLATES see `org-roam-capture-'.
+
+NOTE: This is a copy of the code from the original `org-roam-node-insert'."
+  (interactive current-prefix-arg)
+  (let ((node (org-roam-node-read initial-input filter-fn)))
+    (if (org-roam-node-file node)
+        (org-roam-node-visit node other-window)
+      (org-roam-capture-
+       :node node
+       :templates templates
+       :props '(:finalize find-file)))))
+
+(advice-add 'org-roam-node-find :override #'jnf/org-roam-node-find)
+;; END org-roam overrides
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun jnf/org-roam--thel-sector--capture (&optional goto keys)
+  "As `jnf/org-roam-capture' but scoped to thel-sector project.
+
+Arguments GOTO and KEYS see `org-capture'."
+  (interactive "P")
+  (org-roam-capture goto
+                    keys
+                    :filter-fn (lambda (node) (-contains-p (org-roam-node-tags node) "thel-sector"))
+                    :templates (jnf/org-roam-templates-for :thel-sector)))
+
+(defun jnf/org-roam--thel-sector--node-insert ()
+  "As `org-roam-insert-node' but scoped to thel-sector project."
+  (interactive)
+  (org-roam-node-insert (lambda (node) (-contains-p (org-roam-node-tags node) "thel-sector"))
+                        :templates (jnf/org-roam-templates-for :thel-sector)))
+
+
+(defun jnf/org-roam--thel-sector--node-find (&optional other-window initial-input)
+  "As `org-roam-find-node' but scoped to thel-sector project.
+
+Arguments INITIAL-INPUT and OTHER-WINDOW are from `org-roam-find-mode'."
+  (interactive current-prefix-arg)
+  (org-roam-node-find other-window
+                      initial-input
+                      (lambda (node) (-contains-p (org-roam-node-tags node) "thel-sector"))
+                      :templates (jnf/org-roam-templates-for :thel-sector)))
 
 ;; With the latest update of org-roam, things again behavior
 ;; correctly.  Now I can just load org-roam as part of my day to day
