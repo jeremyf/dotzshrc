7b256820fe57187d525c1144056649493e2a2e32
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-08-02 09:46:06 -0400

    Tidying up emacs config for legibility

--- emacs/jnf-config.el
+++ emacs/jnf-config.el
@@ -7,31 +7,34 @@
 ;;; Code:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(setq jnf/data-directories '(
-                             ;; The themes directory
-                             "~/git/takeonrules.github.io/themes/hugo-tufte"
-                             ;; The content directory
-                             "~/git/takeonrules.github.io/"
-                             ;; The tooling directory
-                             "~/git/dotzshrc/"
-                             ;; The personal configuration options
-                             "~/git/jnf-emacs-config/"
-                             ;; An org directory
-                             "~/git/org/"
-                             ;; An org directory
-                             "~/git/org/archive"
-                             ;; An org directory
-                             "~/git/org/daily"
-                             ;; An org directory
-                             "~/git/org/public"
-                             ;; An org directory
-                             "~/git/org/personal"
-                             ;; An org directory
-                             "~/git/org/hesburgh-libraries"
-                             ))
+(setq jnf/data-directories
+      '(
+        ;; The themes directory
+        "~/git/takeonrules.github.io/themes/hugo-tufte"
+        ;; The content directory
+        "~/git/takeonrules.github.io/"
+        ;; The tooling directory
+        "~/git/dotzshrc/"
+        ;; The personal configuration options
+        "~/git/jnf-emacs-config/"
+        ;; An org directory
+        "~/git/org/"
+        ;; An org directory
+        "~/git/org/archive"
+        ;; An org directory
+        "~/git/org/daily"
+        ;; An org directory
+        "~/git/org/public"
+        ;; An org directory
+        "~/git/org/personal"
+        ;; An org directory
+        "~/git/org/hesburgh-libraries"
+        ))
 
-(cl-defun jnf/git-data-sync (&optional (directories jnf/data-directories ))
-  "Synchronize DIRECTORIES with git pull/push."
+(cl-defun jnf/git-data-sync (&optional (directories jnf/data-directories))
+  "Synchronize DIRECTORIES with git pull/push.
+
+By default the DIRECTORIES are `jnf/data-directories'"
   (interactive)
   (message "Synchronizing local git repos...")
   (dolist (path directories)
@@ -47,8 +50,10 @@
     (message "Finished synchronizing local git repos.")))
 
 
-(cl-defun jnf/git-data-statuses (&optional (directories jnf/data-directories ))
-  "Review DIRECTORIES via `magit'."
+(cl-defun jnf/git-data-statuses (&optional (directories jnf/data-directories))
+  "Review DIRECTORIES via `magit'.
+
+By default the DIRECTORIES are `jnf/data-directories'"
   (interactive)
   (message "Review status of local git repos...")
     (dolist (path directories)
