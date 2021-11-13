9f49d8dc90e3691da2e49fe5607eb99af15487b9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-11-13 08:24:12 -0500

    Updating emacs editing

--- symlinks/.hammerspoon/init.lua
+++ symlinks/.hammerspoon/init.lua
@@ -1,8 +1,22 @@
 local meh = {"ctrl", "alt", "cmd"}
+local hyper = {"ctrl", "alt", "cmd", "shift"}
 
 hs.loadSpoon("editWithEmacs")
-hs.loadSpoon("MiroWindowsManager")
 
+  -- Create the keybindings for editing:
+  --
+  -- Edit by selecting everything in the current focus.
+  hs.hotkey.bind(meh, 'e', nil, function()
+     edit_in_emacs(true)
+  end)
+
+  -- Edit by using only the selected text of the current focus.
+  hs.hotkey.bind(hyper, 'e', nil, function()
+     edit_in_emacs(false)
+  end)
+
+
+hs.loadSpoon("MiroWindowsManager")
 hs.window.animationDuration = 0.3
 spoon.MiroWindowsManager:bindHotkeys({
   up = {meh, "up"},
