d07055c8da43a1cd6613f8296b8ce6ecba257e3c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-11-13 08:42:35 -0500

    Adding configuration to editWithEmacs

--- symlinks/.hammerspoon/init.lua
+++ symlinks/.hammerspoon/init.lua
@@ -2,26 +2,27 @@ local meh = {"ctrl", "alt", "cmd"}
 local hyper = {"ctrl", "alt", "cmd", "shift"}
 
 hs.loadSpoon("editWithEmacs")
+editWithEmacs.editBeginCommand = "editor -e '(hammerspoon-edit-begin)'"
 
-  -- Create the keybindings for editing:
-  --
-  -- Edit by selecting everything in the current focus.
-  hs.hotkey.bind(meh, 'e', nil, function()
-     edit_in_emacs(true)
-  end)
+-- Create the keybindings for editing:
+--
+-- Edit by selecting everything in the current focus.
+hs.hotkey.bind(meh, 'e', nil, function()
+                  edit_in_emacs(true)
+end)
 
-  -- Edit by using only the selected text of the current focus.
-  hs.hotkey.bind(hyper, 'e', nil, function()
-     edit_in_emacs(false)
-  end)
+-- Edit by using only the selected text of the current focus.
+hs.hotkey.bind(hyper, 'e', nil, function()
+                  edit_in_emacs(false)
+end)
 
 
 hs.loadSpoon("MiroWindowsManager")
 hs.window.animationDuration = 0.3
 spoon.MiroWindowsManager:bindHotkeys({
-  up = {meh, "up"},
-  right = {meh, "right"},
-  down = {meh, "down"},
-  left = {meh, "left"},
-  fullscreen = {meh, "="}
+      up = {meh, "up"},
+      right = {meh, "right"},
+      down = {meh, "down"},
+      left = {meh, "left"},
+      fullscreen = {meh, "="}
 })
