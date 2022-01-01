b65ae00bb404b61069ea199c454a0bf63d46d6d4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-12-31 23:05:08 -0500

    Updating ruby config install options

--- configs/paths.zsh
+++ configs/paths.zsh
@@ -31,7 +31,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
     # This helps in building out Ruby using openssl
     # It may require PATH-ing $HB_PATH/Cellar/openssl@1.1/bin (see below)
-    # export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+    export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
 
     # I'm not certain if this is a good idea or not
     if [ -d $HB_PATH/opt/openssl@1.1/bin ]; then
