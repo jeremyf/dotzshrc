1b284b327013c5ccf3dd56996850cdc4fbf1be70
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-03-02 13:21:18 -0500

    Moving FZF configuration to common file

--- configs/config.zsh
+++ configs/config.zsh
@@ -95,31 +95,5 @@ export NVM_DIR="$HOME/.nvm"
 # In my local tests, bat with "ansi" works best for my color schemes
 export BAT_THEME="ansi"
 
-# See https://github.com/junegunn/fzf#environment-variables
-export FZF_DEFAULT_OPTS="--layout=reverse-list --marker=+ --bind 'ctrl-k:kill-line'"
-export FZF_CTRL_T_COMMAND="fd --type f ."
-export FZF_CTRL_T_OPTS="
-  --preview 'bat -n --color=always {}'
-  --height 80%
-  --bind 'ctrl-e:execute(editor {})'
-  --bind 'ctrl-y:execute-silent(echo {} | pbcopy)'
-  --bind 'ctrl-s:execute-silent(echo {} | e-send)'
-  --bind 'ctrl-/:change-preview-window(down|hidden|)'
-  --header 'Press CTRL-e to open in EDITOR; CTRL-y to copy to clipboard'"
-export FZF_CTRL_R_OPTS="
-  --preview 'echo {}' --preview-window up:3:hidden:wrap
-  --bind 'ctrl-/:toggle-preview'
-  --height 50%
-  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)'
-  --color header:italic
-  --header 'Press CTRL-y to copy command into clipboard'"
-export FZF_ALT_C_COMMAND="fd --type d . $HOME"
-export FZF_ALT_C_OPTS="
-  --preview 'tree -C {}'
-  --bind 'ctrl-e:execute(editor {})'
-  --bind 'ctrl-y:execute-silent(echo {} | pbcopy)'
-  --bind 'ctrl-s:execute-silent(echo {} | e-send)'
-  --header 'Press CTRL-e to open in EDITOR; CTRL-y to copy to clipboard'"
-
 # Spring in Rails just causes endless grief.  Disable it
 export DISABLE_SPRING=1
--- symlinks/.fzf.zsh
+++ symlinks/.fzf.zsh
@@ -11,3 +11,29 @@ fi
 # Key bindings
 # ------------
 source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+
+# See https://github.com/junegunn/fzf#environment-variables
+export FZF_DEFAULT_OPTS="--layout=reverse-list --marker=+ --bind 'ctrl-k:kill-line'"
+export FZF_CTRL_T_COMMAND="fd --type f ."
+export FZF_CTRL_T_OPTS="
+  --preview 'bat -n --color=always {}'
+  --height 80%
+  --bind 'ctrl-e:execute(editor {})'
+  --bind 'ctrl-y:execute-silent(echo {} | pbcopy)'
+  --bind 'ctrl-s:execute-silent(echo {} | e-send)'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'
+  --header 'Press CTRL-e to open in EDITOR; CTRL-y to copy to clipboard'"
+export FZF_CTRL_R_OPTS="
+  --preview 'echo {}' --preview-window up:3:hidden:wrap
+  --bind 'ctrl-/:toggle-preview'
+  --height 50%
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)'
+  --color header:italic
+  --header 'Press CTRL-y to copy command into clipboard'"
+export FZF_ALT_C_COMMAND="fd --type d . $HOME"
+export FZF_ALT_C_OPTS="
+  --preview 'tree -C {}'
+  --bind 'ctrl-e:execute(editor {})'
+  --bind 'ctrl-y:execute-silent(echo {} | pbcopy)'
+  --bind 'ctrl-s:execute-silent(echo {} | e-send)'
+  --header 'Press CTRL-e to open in EDITOR; CTRL-y to copy to clipboard'"
